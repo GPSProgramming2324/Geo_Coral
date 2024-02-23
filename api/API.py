6d96cd6b080f6e933a906032459b52bc2772add2
miguelucas12 <miguelucas1207@.com>
2024-02-23 15:25:44 +0000

    reorganizing

--- api/API.py
+++ api/API.py
@@ -21,7 +21,7 @@ try:
 except Exception as e:
     print(f"Error connecting to database: {e}")
 
-@app.route('/Corals/<int:id>', methods=['GET'])
+"""@app.route('/Corals/<int:id>', methods=['GET'])
 def get_Coral(id):
     try:
         cursor.execute("SELECT * FROM Coral WHERE ID = %s", (id,))
@@ -30,7 +30,7 @@ def get_Coral(id):
         if coral:
             coral_dict = {
                 'CORAL_ID': coral[0],
-                'Geometry': coral[1],   
+                'Geometry': coral[1], 
             }
             return jsonify(coral_dict)
         else:
@@ -38,6 +38,34 @@ def get_Coral(id):
 
     except Exception as e:
         return jsonify({'error': f"Error fetching coral data: {e}"}), 500
+"""
+
+
+
+@app.route('/Corals/<int:id>', methods=['GET'])
+def get_Coral(id):
+    try:
+        cursor.execute("SELECT * FROM temperature WHERE ID = %s", (id,))
+        corals = cursor.fetchall()
+
+        coral_list = []
+
+        for coral in corals:
+            coral_dict = {
+                'CORAL_ID':   coral[1],
+                'Date':       coral[2],
+                'Temperature':coral[3]
+            }
+            coral_list.append(coral_dict)
+
+        if coral_list:
+            return jsonify(coral_list)
+        else:
+            return jsonify({'message': 'Coral not found'}), 404
+
+    except Exception as e:
+        return jsonify({'error': f"Error fetching coral data: {e}"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
