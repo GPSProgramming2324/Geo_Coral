2bac57a6eff468fc52432399031bf0830b7edf79
miguelucas12 <miguelucas1207@.com>
2024-02-27 20:25:36 +0000

    pdates

--- api/API.py
+++ api/API.py
@@ -1,5 +1,7 @@
 import psycopg2
 from flask import Flask, request, jsonify
+import json
+import folium
 
 app = Flask(__name__)
 
@@ -25,19 +27,35 @@ except Exception as e:
 @app.route('/Corals/<int:id>', methods=['GET'])
 def get_Coral(id):
     try:
-        cursor.execute("SELECT * FROM temperature WHERE ID = %s", (id,))
+        cursor.execute("""SELECT t.ID, t.Coral_ID, t.Date, t.Temperature, t.DHW, ST_AsGeoJSON(c.Geometry) 
+                        FROM Temperature t
+                        JOIN Coral c ON t.Coral_ID = c.ID
+                        WHERE coral_id = %s;""", (id,))
         corals = cursor.fetchall()
 
         coral_list = []
 
         for coral in corals:
             coral_dict = {
-                'CORAL_ID':   coral[1],
+                'Coral_ID':   coral[1],
                 'Date':       coral[2],
                 'Temperature':coral[3],
-                'DHW':        coral[4]
+                'DHW':        coral[4],
+                'Geometry':   json.loads(coral[5])
             }
             coral_list.append(coral_dict)
+
+            # Create Folium map centered around Indonesia with a dark background
+            m = folium.Map(location=[-7.5489, 131.0149], zoom_start=10, tiles='CartoDB dark_matter')
+
+            for result in corals:
+                folium.GeoJson(
+                    result[5], name=f"Coral_{result[1]}",
+                    tooltip=f"Coral ID: {result[1]}, Temperature: {result[3]}, Date: {result[2]}, DHW: {result[4]}"
+                ).add_to(m)
+
+        # Save the map to an HTML file
+        m.save('corals.html')            
 
         if coral_list:
             return jsonify(coral_list)
@@ -46,6 +64,51 @@ def get_Coral(id):
 
     except Exception as e:
         return jsonify({'error': f"Error fetching coral data: {e}"}), 500
+
+@app.route('/Corals_by_DHW/<int:DHW>', methods=['GET'])
+def get_Coral_by_DHW(DHW):
+    try:
+        cursor.execute("""
+                        SELECT t.ID, t.Coral_ID, t.Date, t.Temperature, t.DHW, ST_AsGeoJSON(c.Geometry) 
+                        FROM Temperature t
+                        JOIN Coral c ON t.Coral_ID = c.ID
+                        WHERE t.DHW > %s;""", (DHW,))
+        corals = cursor.fetchall()
+
+        coral_list = []
+
+        for coral in corals:
+            coral_dict = {
+                'Coral_ID':   coral[1],
+                'Date':       coral[2],
+                'Temperature':coral[3],
+                'DHW':        coral[4],
+                'Geometry':   json.loads(coral[5])
+            }
+            coral_list.append(coral_dict)
+
+            # Create Folium map centered around Indonesia with a dark background
+            m = folium.Map(location=[-7.5489, 131.0149], zoom_start=10, tiles='CartoDB dark_matter')
+
+            for result in corals:
+                folium.GeoJson(
+                    result[5], name=f"Coral_{result[1]}",
+                    tooltip=f"Coral ID: {result[1]}, Temperature: {result[3]}, Date: {result[2]}, DHW: {result[4]}"
+                ).add_to(m)
+
+        # Save the map to an HTML file
+        m.save('corals.html')
+
+        
+        if coral_list:
+            return jsonify(coral_list)
+        else:
+            return jsonify({'message': 'Coral not found'}), 404
+
+    except Exception as e:
+        return jsonify({'error': f"Error fetching coral data: {e}"}), 500
+
+
 
 
 @app.route('/CoralsByDate', methods=['GET'])
@@ -57,18 +120,37 @@ def get_corals_by_date():
         if not (start_date and end_date):
             return jsonify({'error': 'Both start_date and end_date are required parameters'}), 400
 
-        cursor.execute("SELECT * FROM temperature WHERE Date BETWEEN %s AND %s", (start_date, end_date))
+        cursor.execute("""
+                        SELECT t.ID, t.Coral_ID, t.Date, t.Temperature, t.DHW, ST_AsGeoJSON(c.Geometry) 
+                        FROM Temperature t
+                        JOIN Coral c ON t.Coral_ID = c.ID
+                        WHERE Date BETWEEN %s AND %s""", (start_date, end_date))
+        
         corals = cursor.fetchall()
 
-        if not corals:
-            return jsonify({'message': 'No corals found within the specified date range'}), 404
+        coral_list = []
 
-        coral_list = [{
-            'CORAL_ID': coral[1],
-            'Date': coral[2],
-            'Temperature': coral[3],
-            'DHW':coral[4]
-        } for coral in corals]
+        for coral in corals:
+            coral_dict = {
+                'Coral_ID':   coral[1],
+                'Date':       coral[2],
+                'Temperature':coral[3],
+                'DHW':        coral[4],
+                'Geometry':   json.loads(coral[5])
+            }
+            coral_list.append(coral_dict)
+
+            # Create Folium map centered around Indonesia with a dark background
+            m = folium.Map(location=[-7.5489, 131.0149], zoom_start=10, tiles='CartoDB dark_matter')
+
+            for result in corals:
+                folium.GeoJson(
+                    result[5], name=f"Coral_{result[1]}",
+                    tooltip=f"Coral ID: {result[1]}, Temperature: {result[3]}, Date: {result[2]}, DHW: {result[4]}"
+                ).add_to(m)
+
+        # Save the map to an HTML file
+        m.save('corals.html')
 
         return jsonify(coral_list)
 
@@ -77,6 +159,3 @@ def get_corals_by_date():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
